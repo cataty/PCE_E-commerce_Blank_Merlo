a59bb0172c9317cd2bc730ff47e04ed3466d97f2
Tatjana <83314427+cataty@users.noreply.github.com>
2025-05-01 19:10:36 -0300

    moviendo recursos a 'public', agregando estilos y bloque "quienes somos"

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,28 +19,28 @@
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="/img/logo.png" class="h-8" alt="Monstierra Logo" />
     </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-lightgreen dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Abrir menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-lightgreen">
         <li>
-          <a class="block py-2 px-3 text-white bg-pink-900 rounded-sm md:bg-transparent md:text-pink-900 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page" href="/">Home</a>
+          <a class="block py-2 px-3 text-white bg-dark-orange rounded-sm md:bg-transparent md:text-dark-orange md:p-0 dark:text-white md:dark:text-light-green" aria-current="page" href="/">Home</a>
         </li>
         <li>
-          <a class="block py-2 px-3 text-sky-900 rounded-sm hover:bg-sky-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-sky-700 dark:hover:text-white md:dark:hover:bg-transparent" href="#about">Quiénes Somos</a>
+          <a class="block py-2 px-3 text-darkgreen rounded-sm hover:bg-emerald-100 md:hover:bg-transparent md:border-0 md:hover:text-dark-orange md:p-0 dark:text-white md:dark:hover:text-lightgreen dark:hover:bg-lightgreen dark:hover:text-white md:dark:hover:bg-transparent" href="#about">Quiénes Somos</a>
         </li>
         <li>
-          <a class="block py-2 px-3 text-sky-900 rounded-sm hover:bg-sky-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-sky-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/productos/todos">Productos</a>
+          <a class="block py-2 px-3 text-darkgreen rounded-sm hover:bg-emerald-100 md:hover:bg-transparent md:border-0 md:hover:text-dark-orange md:p-0 dark:text-white md:dark:hover:text-lightgreen dark:hover:bg-lightgreen dark:hover:text-white md:dark:hover:bg-transparent" href="/productos/todos">Productos</a>
         </li>
         <li>
-          <a class="block py-2 px-3 text-sky-900 rounded-sm hover:bg-sky-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-sky-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/blog/todos">Blog</a>
+          <a class="block py-2 px-3 text-darkgreen rounded-sm hover:bg-emerald-100 md:hover:bg-transparent md:border-0 md:hover:text-dark-orange md:p-0 dark:text-white md:dark:hover:text-lightgreen dark:hover:bg-lightgreen dark:hover:text-white md:dark:hover:bg-transparent" href="/blog/todos">Blog</a>
         </li>
         <li>
-          <a class="block py-2 px-3 text-sky-900 rounded-sm hover:bg-sky-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href='/login'>Iniciar Sesión</a>
+          <a class="block py-2 px-3 text-darkgreen rounded-sm hover:bg-emerald-100 md:hover:bg-transparent md:border-0 md:hover:text-dark-orange md:p-0 dark:text-white md:dark:hover:text-lightgreen dark:hover:bg-lightgreen dark:hover:text-white md:dark:hover:bg-transparent" href='/login'>Iniciar Sesión</a>
         </li>
       </ul>
     </div>
@@ -50,7 +50,7 @@
         <main class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {{$slot}}
         </main>
-        <footer class="footer text-bg-dark color-sky-900 text-center">
+        <footer class="footer text-lightorange bg-darkgreen text-center">
         <p>Copyright &copy; Da Vinci 2024</p>
         </footer>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,45 @@
 <x-layout>
 <x-slot:title>Home</x-slot:title>
 
- <section class="bg-white dark:bg-gray-900">
+    <section class="bg-white dark:bg-gray-900">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7 color-sky-900">
-            <h1 class="max-w-2xl mb-4 text-4xl text-white font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Imaginación sin limites</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
-            <a href="#" class="inline-flex item">
+        <div class="lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="{{ asset('img/illu_main.svg') }}" alt="Illustration" class="w-full h-full object-cover">
+        </div>    
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl text-darkorange font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Imaginación sin limites</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                 En Monstierra, cada camiseta es una obra de arte única. Cada prenda combina arte y originalidad, reflejando la pasión por lo diferente y lo hecho a mano. ¡Perfecta para quienes buscan destacar con estilo!
+            </p>
+            <a href="/Productos" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Ver Productos
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Speak to Sales
             </a> 
         </div>
-        <div class="lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
-        </div>                
+            
     </div>
 </section>
+<x-shopPreview :products="$products" />
+</x-shopPreview>
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+    <section class="bg-lightgreen dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div class="mr-auto place-self-center lg:col-span-7">
+                <h2 class="max-w-2xl mb-4 text-4xl text-lightorange font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Productos destacados</h2>
+                <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                </p>
+            </div>
+        </div>
 
-</x-layout>s-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Get started 
-
-<h1>hola</h1>
+<section class="bg-lightgreen dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7" id="about">
+            <h2 class="max-w-2xl mb-4 text-4xl text-lightorange font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Acerca de Nosotros</h2>
+            <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                Somos una startup artesanal con 5 años de experiencia en la creación de camisetas únicas y originales. Nuestros diseños están inspirados en la creatividad y la pasión por lo diferente, destacando por su estilo auténtico. Cada prenda refleja nuestro compromiso con el arte independiente y la calidad hecha a mano. Durante estos años, hemos crecido gracias a quienes valoran la originalidad y buscan destacar con personalidad. ¡Únete a nuestra comunidad y lleva contigo una pieza de imaginación sin límites!
+            </p>
+        </div>
+    </div>
+</section>
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+</x-layout>
