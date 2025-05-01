0d9f189b83d813484010f1278ca7db06dfa625c6
Tatjana <83314427+cataty@users.noreply.github.com>
2025-05-01 04:19:32 -0300

    agregando rutas para los controllers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'] ); // dentro de la classe HomeController, buscamos el metodo index y la ejecutamos cuando se accede a la ruta /
+
+Route::get('/contact', [ContactController::class, 'viewContact']); // dentro de la classe ContactController, buscamos el metodo viewContact y la ejecutamos cuando se accede a la ruta /contact
+
+Route::get('/productos', [ProductosController::class, 'viewProductos']); // dentro de la classe ProductosController, buscamos el metodo viewProductos y la ejecutamos cuando se accede a la ruta /productos  
+
+Route::get('/productos/{id}', function ($id) { // se puede pasar un parametro como una id mediante la ruta
+    echo "El id del producto es: $id";
 });
+
+Route::get('/blog', [BlogController::class, 'viewBlog']); // dentro de la classe BlogController, buscamos el metodo viewBlog y la ejecutamos cuando se accede a la ruta /blog
+
+Route::get('/blog/{id}', function ($id) { // se puede pasar un parametro como una id mediante la ruta
+    echo "El id del articulo es: $id";
+});
+
+
