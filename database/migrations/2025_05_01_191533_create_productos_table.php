fd9a217140bec32cea7c163164b81cf86894d3f1
maca merlo <macarena.merlo@gmail.com>
2025-05-01 17:44:59 -0300

    base de datos 'monsterra creada'
    Tablas:
    -productos
    -categorias-pr (para productos)
    -users
    -blog

---
+++ database/migrations/2025_05_01_191533_create_productos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id('producto_id');
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->integer('precio')->unsigned();
+            $table->string('categoria',100);
+            $table->string('imagen'); // nullable() permite que el campo imagen pueda ser nulo
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
