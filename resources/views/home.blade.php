e41ea84ea04c022ba1e5918f36b417b27a2a10a2
Tatjana <83314427+cataty@users.noreply.github.com>
2025-05-01 19:23:07 -0300

    agregando cards al preview

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,8 +19,7 @@
             
     </div>
 </section>
-<x-preview :products="$products" />
-</x-preview>
+<x-preview />
 <section class="bg-lightgreen dark:bg-gray-900">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7" id="about">
