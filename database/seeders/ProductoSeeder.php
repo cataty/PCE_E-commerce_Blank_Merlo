30ffd263d103d16f90d8d38cd83df666b9336958
maca merlo <macarena.merlo@gmail.com>
2025-05-01 20:42:17 -0300

    productos cargados en la BD a traves del seeder

---
+++ database/seeders/ProductoSeeder.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('productos')->insert([
+            [
+                'producto_id' => 1,
+                'nombre' => 'Remera Monster Enough',
+                'descripcion' => 'Remera gris melange con un adorable monstruito y el inspirador mensaje "You Are Enough"',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_enough_gris.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 2,
+                'nombre' => 'Remera Monster Enough',
+                'descripcion' => 'Remera marron con un adorable monstruito y el inspirador mensaje "You Are Enough"',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_enough_marron.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 3,
+                'nombre' => 'Remera Monster Enough',
+                'descripcion' => 'Remera verde con un adorable monstruito y el inspirador mensaje "You Are Enough"',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_enough_verde.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 4,
+                'nombre' => 'Remera Honguito',
+                'descripcion' => 'Remera gris melange con un diseño abstracto y psicodélico de un hongo. ¡Un toque de originalidad para tu look!',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_hongo_gris.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 5,
+                'nombre' => 'Remera Honguito',
+                'descripcion' => 'Remera marron con un diseño abstracto y psicodélico de un hongo. ¡Un toque de originalidad para tu look!',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_hongo_marron.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 6,
+                'nombre' => 'Remera Honguito',
+                'descripcion' => 'Remera verde con un diseño abstracto y psicodélico de un hongo. ¡Un toque de originalidad para tu look!',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_hongo_verde.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 7,
+                'nombre' => 'Remera Monster Hungry',
+                'descripcion' => 'Remera gris melange con un monstruito de un ojo que dice "hello im hungry". ¡Perfecta para los que siempre tienen apetito con un toque de humor! (Como nosotros)',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_hungry_gris.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 8,
+                'nombre' => 'Remera Monster Hungry',
+                'descripcion' => 'Remera marron con un monstruito de un ojo que dice "hello im hungry". ¡Perfecta para los que siempre tienen apetito con un toque de humor! (Como nosotros)',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_hungry_marron.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 9,
+                'nombre' => 'Remera Monster Hungry',
+                'descripcion' => 'Remera verde con un monstruito de un ojo que dice "hello im hungry". ¡Perfecta para los que siempre tienen apetito con un toque de humor! (Como nosotros)',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_hungry_verde.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 10,
+                'nombre' => 'Remera Monster Marco',
+                'descripcion' => 'Esta remera gris captura una escena curiosa: un pequeño monstruo parece asomarse o estar atrapado en un marco. Su diseño expresivo y llamativo seguramente generará conversaciones.',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_marco_gris.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 11,
+                'nombre' => 'Remera Monster Marco',
+                'descripcion' => 'Esta remera marron captura una escena curiosa: un pequeño monstruo parece asomarse o estar atrapado en un marco. Su diseño expresivo y llamativo seguramente generará conversaciones.',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_marco_marron.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 12,
+                'nombre' => 'Remera Monster Marco',
+                'descripcion' => 'Esta remera verde captura una escena curiosa: un pequeño monstruo parece asomarse o estar atrapado en un marco. Su diseño expresivo y llamativo seguramente generará conversaciones.',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_marco_verde.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 13,
+                'nombre' => 'Remera Monster Yeah',
+                'descripcion' => 'Remera gris con un fantasma súper cool. ¡Para los que van con la mejor vibra siempre!',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_yeah_gris.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 14,
+                'nombre' => 'Remera Monster Yeah',
+                'descripcion' => 'Remera marron con un fantasma súper cool. ¡Para los que van con la mejor vibra siempre!',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_yeah_marron.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'producto_id' => 15,
+                'nombre' => 'Remera Monster Yeah',
+                'descripcion' => 'Remera verde con un fantasma súper cool. ¡Para los que van con la mejor vibra siempre!',
+                'precio' => 5000,
+                'categoria' => 'Remeras',
+                'imagen' => 'img/productos/t-shirt_yeah_verde.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+    ]);
+    }
+}
