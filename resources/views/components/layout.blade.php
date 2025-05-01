765df14b31764b4ce0124533f1083b7aea8856a8
Tatjana <83314427+cataty@users.noreply.github.com>
2025-05-01 05:51:31 -0300

    cambiando colores navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,26 +30,26 @@
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page" href="/">Home</a>
+          <a href="#" class="block py-2 px-3 text-white bg-pink-900 rounded-sm md:bg-transparent md:text-pink-900 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page" href="/">Home</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/about">Quiénes Somos</a>
+          <a href="#" class="block py-2 px-3 text-sky-900 rounded-sm hover:bg-sky-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-sky-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/about">Quiénes Somos</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/productos">Productos</a>
+          <a href="#" class="block py-2 px-3 text-sky-900 rounded-sm hover:bg-sky-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-sky-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/productos">Productos</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/blog">Blog</a>
+          <a href="#" class="block py-2 px-3 text-sky-900 rounded-sm hover:bg-sky-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-sky-700 dark:hover:text-white md:dark:hover:bg-transparent" href="/blog">Blog</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href='/login'>Iniciar Sesión</a>
+          <a href="#" class="block py-2 px-3 text-sky-900 rounded-sm hover:bg-sky-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" href='/login'>Iniciar Sesión</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
 
-        <main class="p-4">
+        <main class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {{$slot}}
         </main>
         <footer class="footer text-bg-dark text-center">
