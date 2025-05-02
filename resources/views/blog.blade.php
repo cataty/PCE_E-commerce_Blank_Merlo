53a940cbfcf68ed1fb1b40efdbba7a45503ae99b
Tatjana <83314427+cataty@users.noreply.github.com>
2025-05-02 13:50:02 -0300

    estilos producto & traer datos del blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,21 +4,30 @@
 
 <x-layout>
 <x-slot:title>Blog</x-slot:title>
-    <h1 class="mb-4 text-4xl text-gray-900 md:text-5xl lg:text-6xl">Blog</h1>
-    @foreach ($blog as $blogpost):
+<div class="flex flex-wrap justify-center gap-4">
+    <h1 class="max-w-2xl mb-12 pt-12 text-4xl text-lightgreen font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-whitel">Blog</h1>
+    <div class="flex flex-wrap justify-center gap-4">
+    @foreach ($blog as $blogpost)
+
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <img class="rounded-t-lg" src="{{ $blogpost->imagen }}" alt="" />
+            <a href="{{ route('blogpost', ['id' => $blogpost->blogpost_id]) }}">
+                <img class="rounded-t-lg" src="{{ asset($blogpost->imagen)}}" alt="" />
             </a>
             <div class="p-5">
-                <a href="#">
-                    <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $blogpost->titulo }}</h3>
+                <a href="{{ route('blogpost', ['id' => $blogpost->blogpost_id]) }}">
+                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-darkgreen dark:text-white">{{ $blogpost->nombre }}</h3>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $blogpost->contenido }}</p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <p class="mb-8 text-lightgreen">precio: {{ $blogpost->precio }}$</p>
+                {{-- <p class="mb-3 font-normal text-lightgreen dark:text-gray-400">{{ $blogpost->descripcion }}</p> --}}
+                {{-- <p class="mb-3 font-normal text-lightgreen dark:text-gray-400">{{ $blogpost->stock }}</p> --}}
+                {{-- <p class="mb-3 font-normal text-lightgreen dark:text-gray-400">{{ $blogpost->descripcion }}</p> --}}
+                <a href="{{ route('blogpost', ['id' => $blogpost->blogpost_id]) }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Ver más
                 </a>
             </div>
         </div>
+
     @endforeach;
+    </div>
+</div>
 </x-layout>
--- resources/views/producto.blade.php
+++ resources/views/producto.blade.php
@@ -1,11 +1,14 @@
 <x-layout>
 <x-slot:title>Producto:</x-slot:title>
-    <div class="w-full sm:w-8/12 lg:w-4/12 mx-auto">
+    <div class="w-full sm:w-8/12 lg:w-4/12 mx-auto my-12">
 
-        <h1 class="mb-4 text-4xl text-gray-900 md:text-5xl lg:text-6xl">{{$producto["nombre"]}}</h1>
-        <p>
-            <img class="rounded-t-lg" src="{{ asset($producto['imagen'])}}" alt="{{$producto['nombre']}}" />
+        <h1 class="mb-4 text-4xl text-gray-900 md:text-4xl">{{$producto["nombre"]}}</h1>
+        <p class=" text-gray-700 dark:text-gray-400">
+            <img class="mb-12" src="{{ asset($producto['imagen'])}}" alt="{{$producto['nombre']}}" />
             {{$producto["descripcion"]}}
+        </p>
+        <p class=" text-lightgreen dark:text-gray-400 mt-4 mb-12">
+            precio: {{$producto["precio"]}}$
         </p>
         <form>
             <button type="submit" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
